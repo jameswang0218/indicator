b72cdc4576e20831d70acb25be6739f3a44726a9
james <james@mail.fameex.info>
2024-09-02 19:39:40 +0400

    鳄鱼线 值修改

--- smma.go
+++ smma.go
@@ -2,20 +2,18 @@ package indicator
 
 // Smma 结构体，包含平滑类型和历史位数
 type Smma struct {
-	period    int32
-	prices    []float64
-	smma      float64
-	isInitial bool
-	offset    int32 // 用于指定要获取的历史位数
-	age       uint32
+	period int32
+	prices []float64
+	smma   float64
+	offset int32 // 用于指定要获取的历史位数
+	age    uint32
 }
 
 // NewSmma 创建一个新的 SMMA 实例，支持历史位数
 func NewSmma(period int32, offset int32) *Smma {
 	return &Smma{
-		period:    period,
-		isInitial: true,
-		offset:    offset,
+		period: period,
+		offset: offset,
 	}
 }
 
@@ -73,11 +71,11 @@ func (s *Smma) GetFutureSegment() []float64 {
 // Clone 创建当前 SMMA 实例的深拷贝
 func (s *Smma) Clone() *Smma {
 	clone := &Smma{
-		period:    s.period,
-		prices:    append([]float64{}, s.prices...), // 深拷贝价格数据
-		smma:      s.smma,
-		isInitial: s.isInitial,
-		offset:    s.offset,
+		period: s.period,
+		prices: append([]float64{}, s.prices...), // 深拷贝价格数据
+		smma:   s.smma,
+		offset: s.offset,
+		age:    s.age,
 	}
 	return clone
 }
